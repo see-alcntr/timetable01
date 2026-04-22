87c777d9ae2bb500c1dbb9a9fd286df5311f8abc
see-alcntr <alcantaravsee@gmail.com>
2026-04-22 14:12:40 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,15 @@
-from flask import Flask, render_template, request, redirect, session
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "✅ Timetable System is LIVE on Render!"
+
+if __name__ == "__main__":
+    app.run()
+    
+    from flask import Flask, render_template, request, redirect, session
 import sqlite3
 
 app = Flask(__name__)
